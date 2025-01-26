91bb7f1edcedab8eae93def513a57e82001e855c
Sergio Heredia <sergioheredia1996@gmail.com>
2025-01-25 16:04:40 -0800

    completed all tests

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    result = "That will be twenty bucks."
+  } else if (feet > 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC"? "Ok, sounds good.": "No go." 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch(tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye."
+      break
+  }
+
+  return response;
 }
+
+console.log(ternaryCheckCity("Cali"));
+console.log("Cali" === "NYC"? "Ok, sounds good.": "No go." )
